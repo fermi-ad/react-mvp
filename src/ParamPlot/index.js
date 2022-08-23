d3cbb0ada30c1d41a27d5e56465ecae6a56fa718
Beau Harrison <beau@fnal.gov>
2022-08-23 13:43:01 -0500

    Add a legend.

--- src/ParamPlot/index.js
+++ src/ParamPlot/index.js
@@ -35,6 +35,32 @@ export const ParamPlot = ({ drf, points }) => {
       isInteractive={false}
       animate={false}
       enableArea={false}
+      legends={[
+        {
+          anchor: "bottom-right",
+          direction: "column",
+          justify: false,
+          translateX: 0,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemDirection: "left-to-right",
+          itemWidth: 80,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: "circle",
+          symbolBorderColor: "rgba(0, 0, 0, .5)",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemBackground: "rgba(0, 0, 0, .03)",
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
     />
   );
 };
